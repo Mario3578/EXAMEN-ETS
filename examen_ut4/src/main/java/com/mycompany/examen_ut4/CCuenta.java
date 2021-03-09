8765b6838c7554b624214872275ad528891326a4
Mario3578 <77676689+Mario3578@users.noreply.github.com>
2021-03-09 09:45:05 +0100

    Add files via upload

---
+++ examen_ut4/src/main/java/com/mycompany/examen_ut4/CCuenta.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.examen_ut4;
+
+/**
+ *
+ * @author 1cfgs
+ */
+public class CCuenta {
+
+    public String nombre;
+    /** Primer String */
+    public String cuenta;
+    /** Segundo String */
+    public double saldo;
+    /** primer public */
+    public double tipoInterÃ;
+    /** Segundo interface */ 
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+    }
+
+    public void asignarNombre(String nom)
+    {
+        setNombre(nom);
+    }
+
+    public String obtenerNombre()
+    {
+        return getNombre();
+    }
+
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+
+    public String obtenerCuenta()
+    {
+        return getCuenta();
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public double getTipoInterÃ() {
+        return tipoInterÃ;
+    }
+
+
+    public void setTipoInterÃ(double tipoInterÃ) {
+        this.tipoInterÃ = tipoInterÃ;
+    }
+}
---
+++ examen_ut4/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"com.mycompany.examen_ut4","l":"CCuenta"},{"p":"com.mycompany.examen_ut4","l":"Main"}];updateSearchResults();
